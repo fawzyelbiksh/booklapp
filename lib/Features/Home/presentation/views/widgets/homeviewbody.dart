d697754d5f2063dc6640c1ef17df41d6735bf9b2
elbikshfawzy@gmail <elbikshfawzy@gmail.com>
2026-09-01 19:40:38 +0300

    Create home repo

--- lib/Features/Home/presentation/views/widgets/homeviewbody.dart
+++ lib/Features/Home/presentation/views/widgets/homeviewbody.dart
@@ -2,7 +2,6 @@ import 'package:booklapp/Core/utils/styles.dart';
 import 'package:booklapp/Features/Home/presentation/views/widgets/bestsellerlistview.dart';
 import 'package:flutter/material.dart';
 import 'CustomAppBar.dart';
-import 'featured_book_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
