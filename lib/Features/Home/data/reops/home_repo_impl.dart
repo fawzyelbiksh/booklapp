c26a8436548863167152f38bda9f7f5937203fc1
Khalid-MahmouD <khalooood827@gmail.com>
2026-09-03 20:37:53 +0300

    create type of failure and  implement FetchFeaturedBooks

--- lib/Features/Home/data/reops/home_repo_impl.dart
+++ lib/Features/Home/data/reops/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:booklapp/Core/utils/api_service.dart';
 import 'package:booklapp/Features/Home/data/models/book_model/book_model.dart';
 import 'package:booklapp/Features/Home/data/reops/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   ApiService apiService;
@@ -10,8 +11,8 @@ class HomeRepoImpl implements HomeRepo {
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
-  try {
-    var data = await     apiService.get(
+    try {
+      var data = await apiService.get(
         endPoint:
             'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:programming',
       );
@@ -20,13 +21,30 @@ class HomeRepoImpl implements HomeRepo {
         books.add(BookModel.fromJson(item));
       }
       return right(books);
-  }  catch (e) {
-    return left(ServerFailure());
-  }
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming',
+      );
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
