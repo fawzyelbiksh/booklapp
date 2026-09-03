a36bfb4bf2301640ab35abd88beab07ba758cc42
Khalid-MahmouD <khalooood827@gmail.com>
2026-09-03 21:17:48 +0300

    newestBookCubit done

---
+++ lib/Features/Home/presentation/manager/Newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:bloc/bloc.dart';
+import 'package:booklapp/Features/Home/data/models/book_model/book_model.dart';
+import 'package:booklapp/Features/Home/data/reops/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit({required this.homeRepo}) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold(
+      (failure) => emit(NewestBooksFailure(failure.errMessage)),
+      (books) => emit(NewestBooksSuccess(books)),
+    );
+  }
+}
